9407376f40e3335078e175e6b79ef3e79658ffff
Dion Pettaway <dionp@fullsail.edu>
2017-06-03 19:54:05 -0400

    Added unit tests for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,5 +99,43 @@ describe('GET /todos/:id', () => {
             .get(`/todos/439ss92s`)
             .expect(404)
             .end(done);
-    })
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('Should remove a todo', (done) => {
+        var hexID = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexID);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexID).then((todo) => {
+                    expect(todo).toNotExist();
+                    done()
+                }).catch((e) => done(e));
+            })
+    });
+
+    it('Should return a 404 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('Should return a 404 if object ID is invalid', (done) => {
+        request(app)
+            .delete('/todos/a93nkj')
+            .expect(404)
+            .end(done);
+    });
 });
