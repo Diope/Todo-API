fd35f1ff7dc9be38270695aad9606d80273468aa
Dion Pettaway <dionp@fullsail.edu>
2017-06-03 22:10:51 -0400

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,37 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, 'password');
+console.log(token)
+
+var decoded = jwt.verify(token, "password");
+console.log(`decoded:`, decoded)
+
+// var message = "This is a user string"
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`)
+
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed! Yatta! 😍');
+// } else {
+//     console.log('Data compromised 🙍🏾‍')
+// }
--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT;
 
 const app = express();
 app.use(bodyParser.json());
@@ -81,6 +82,21 @@ app.patch('/todos/:id', (req, res) => {
         res.send({todo});
     }).catch((e) => {
         res.status(400).send()
+    })
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).done(e);
     })
 });
 
